a313965df95bbb6a2d22f08bb9ed295025cb6c3a
edin.c <edin.c@kakaocorp.com>
2021-10-25 22:59:32 +0900

    cmake-format test

--- cmake/compiler-flag.cmake
+++ cmake/compiler-flag.cmake
@@ -3,48 +3,41 @@
 #
 
 # ---- debug build flag ----
-				set(CLANG_DEBUG_FLAGS
-	-O0
-	-g3
-	-ggdb
-	-fPIC
-	-fexceptions
-	-pthread
-	-fcolor-diagnostics
-)
+		set(CLANG_DEBUG_FLAGS
+    -O0
+    -g3
+    -ggdb
+    -fPIC
+    -fexceptions
+    -pthread
+    -fcolor-diagnostics)
 
 set(GCC_DEBUG_FLAGS
-	-O0 
-	-g3 
-	-ggdb 
-	-fPIC 
-	-fexceptions 
-	-pthread 
-	-fdiagnostics-color=always 
-)
+    -O0
+    -g3
+    -ggdb
+    -fPIC
+    -fexceptions
+    -pthread
+    -fdiagnostics-color=always)
 
-set(MSVC_DEBUG_FLAGS
-)
+set(MSVC_DEBUG_FLAGS)
 
 # ---- release build flag ----
 set(CLANG_RELEASE_FLAGS
-	-O3
-	-DNDEBUG
-	-fPIC
-	-fexceptions
-	-pthread
-	-fcolor-diagnostics
-)
+    -O3
+    -DNDEBUG
+    -fPIC
+    -fexceptions
+    -pthread
+    -fcolor-diagnostics)
 
 set(GCC_RELEASE_FLAGS
-	-O3
-	-DNDEBUG
-	-fPIC
-	-fexceptions
-	-pthread
-	-fdiagnostics-color=always
-)
+    -O3
+    -DNDEBUG
+    -fPIC
+    -fexceptions
+    -pthread
+    -fdiagnostics-color=always)
 
-set(MSVC_RELEASE_FLAGS
-)
-
+set(MSVC_RELEASE_FLAGS)
