805156a9f29dace5fcd889076916073f7fa2a197
edin.c <edin.c@kakaocorp.com>
2021-10-25 23:33:21 +0900

    cmake-format test

--- cmake/documentation.cmake
+++ cmake/documentation.cmake
@@ -11,11 +11,6 @@ set(DOXYGEN_EXTRACT_ALL YES)
 set(DOXYGEN_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR})
 
 find_package(Doxygen REQUIRED dot)
-doxygen_add_docs(
-	doxygen
-	${PROJECT_SOURCE_DIR}
-	COMMENT "Generate man pages"
-)
+doxygen_add_docs(doxygen ${PROJECT_SOURCE_DIR} COMMENT "Generate man pages")
 
 message(STATUS "Doxygen has been setup and documentation is now available.")
-
--- cmake/out-of-source-build.cmake
+++ cmake/out-of-source-build.cmake
@@ -3,10 +3,8 @@
 #
 
 # ---- Include guards ----
-if (PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR)
-	message(
-		FATAL_ERROR
-			"In-source builds not allowd. Please make a new directory (called a build directory) and run CMake from there."
-	)
+if(PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR)
+  message(
+    FATAL_ERROR
+      "In-source builds not allowd. Please make a new directory (called a build directory) and run CMake from there.")
 endif()
-
